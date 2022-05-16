d4c4887cf778882f05cf126d356759d8005e9d66
DeChrigi <schweizerchristian50@gmail.com>
2022-05-16 13:52:22 +0000

    Anpassung Titel

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker",
+            "title": "Requirements Engineering Friend Tracker Christian Schweizer",
             "formList": [
                 {
                     "id": "FriendForm",
